459e1b84aa7810ec27bdc290b272b0ed9bc08cd3
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-04-17 12:50:02 -0300

    first commit

---
+++ application/models/Pages_model.php
@@ -0,0 +1,57 @@
+<?php
+
+class Pages_model extends CI_Model
+{
+    private $table_name = 'pages';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get($id = null)
+    {
+        if ($id === null) {
+            $query = $this->db->get($this->table_name);
+            return $query->result();
+        }
+        $query = $this->db->get_where($this->table_name, ['id'=>$id]);
+        return $query->first_row();
+    }
+
+    public function new()
+    {
+        $this->load->helper('url');
+        $slug = url_title($this->input->post('title'), 'dash', true);
+
+        $data = [
+            'title' => $this->input->post('title'),
+            'body' => $this->input->post('body'),
+            'slug' => $slug
+        ];
+
+        return $this->db->insert($this->table_name, $data);
+    }
+
+    public function update($id)
+    {
+        $this->load->helper('url');
+        $slug = url_title($this->input->post('title'), 'dash', true);
+
+        $data = [
+            'title' => $this->input->post('title'),
+            'body' => $this->input->post('body'),
+            'slug' => $slug
+        ];
+
+        $this->db->where('id', $id);
+
+        return $this->db->update($this->table_name, $data);
+    }
+
+    public function delete($id)
+    {
+        return $this->db->delete($this->table_name, ['id'=>$id]);
+    }
+}
